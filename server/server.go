e431f4372312bb593686f1a0ec369b42f0ec32f3
bsh86 <bsh86@uclive.ac.nz>
2024-08-28 10:34:47 +1200

    created a main page template with a navbar

--- server/server.go
+++ server/server.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-	"io"
+	"html/template"
 	"net/http"
 )
 
 func getMain(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "hello world")
+	t, err := template.ParseFiles("templates/index.html", "templates/navbar.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	t.Execute(w, nil)
 }
 
 func main() {
