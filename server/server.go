158470c5b1ec223f50d51bd6955144397e65fd6a
bsh86 <bsh86@uclive.ac.nz>
2024-08-28 17:33:24 +1200

    started writing bio

--- server/server.go
+++ server/server.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
+	"time"
 )
 
 func getMain(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/index.html", "templates/navbar.html")
+
+	age := time.Now().Year() - 2004
+	if time.Now().Month() == time.January && time.Now().Day() < 5 {
+		age -= 1
+	}
+
+	MainInfo := struct {
+		Age string
+	}{
+		fmt.Sprintf("%d", age),
+	}
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	t.Execute(w, nil)
+	t.Execute(w, MainInfo)
 }
 
 func main() {
