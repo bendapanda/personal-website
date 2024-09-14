564c7abc64f2530484459390645f496a546e3ea6
Ben Shirley <benshirley04@gmail.com>
2024-09-14 15:18:55 +1200

    added database initialisation method

--- server/db/projects.go
+++ server/db/projects.go
@@ -1,25 +1,43 @@
-package db
+package database
 
 import (
 	"database/sql"
 	"os"
 
-	_ "github.com/lib/pq"
 	log "github.com/sirupsen/logrus"
+
+	_ "github.com/lib/pq"
 )
 
-func InitProjectsTable() {
+var db *sql.DB
 
-	database_user := os.Getenv("POSTGRES_USER")
-	database_passwrd := os.Getenv("POSTGRES_PASSWORD")
+type project struct {
+	name        string
+	description string
+}
 
-	database_url := "postgres://" + database_user + ":" + database_passwrd + "@localhost:1234?sslmode=disable"
-	database, err := sql.Open("postgres", database_url)
+// Initialises the database connection
+func InitDatabase() error {
+	postgres_username := os.Getenv("POSTGRES_USER")
+	postgres_password := os.Getenv("POSTGRES_PASSWORD")
+	postgres_url := "postgresql://" + postgres_username + ":" + postgres_password + "@localhost:5432?sslmode=disable"
+	var err error
+	db, err = sql.Open("postgres", postgres_url)
 	if err != nil {
 		log.Fatal(err)
+		return err
 	}
-	log.Info("connected to database")
-	database.Query("SELECT * FROM projects")
 
-	database.Close()
+	return nil
+}
+
+// fetches all projects in the database
+func GetAllProjects() ([]project, error) {
+
+	queryString := "SELECT * FROM projects"
+	rows, err := db.Query(queryString)
+	if err != nil {
+
+	}
+
 }
