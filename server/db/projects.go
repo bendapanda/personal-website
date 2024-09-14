46068b8215431df29cc027d2a2d3af445da2b281
Ben Shirley <benshirley04@gmail.com>
2024-09-14 14:18:04 +1200

    created postgresql database

--- server/db/projects.go
+++ server/db/projects.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"os"
 
 	_ "github.com/lib/pq"
 	log "github.com/sirupsen/logrus"
@@ -9,11 +10,16 @@ import (
 
 func InitProjectsTable() {
 
-	database, err := sql.Open("postgres", "user=bensh dbname=personal_website")
+	database_user := os.Getenv("POSTGRES_USER")
+	database_passwrd := os.Getenv("POSTGRES_PASSWORD")
+
+	database_url := "postgres://" + database_user + ":" + database_passwrd + "@localhost:1234?sslmode=disable"
+	database, err := sql.Open("postgres", database_url)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Info("connected to database")
+	database.Query("SELECT * FROM projects")
 
 	database.Close()
 }
