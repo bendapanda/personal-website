91a917753602645808bfdbbbc956a9758e70c1ba
Ben Shirley <benshirley04@gmail.com>
2024-10-22 14:47:53 +1300

    set up navbar in frontend directory

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/Navbar.css';
+
+
+const Navbar = () => {
+    return (
+        <ul class="navbar section">
+            <li><h1>Ben Shirley</h1></li>
+            <li><a class="navbar-link" href="/">Home</a></li>
+            <li><a class="navbar-link" href="/resume">Resume</a></li>
+            <li><a class="navbar-link" href="https://github.com/bendapanda">Projects</a></li>
+        </ul>
+    );
+};
+
+export default Navbar;
