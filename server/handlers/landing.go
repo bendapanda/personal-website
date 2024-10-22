3c9344ff55a9963e37fa76c9fff2f1923cc5c482
Ben Shirley <benshirley04@gmail.com>
2024-10-22 13:20:25 +1300

    added a few comments

--- server/handlers/landing.go
+++ server/handlers/landing.go
@@ -11,11 +11,14 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+// Called when a user requests the landing page. Dynamically assembles the page using results from the database
 func GetLanding(w http.ResponseWriter, r *http.Request) {
+	// Parse all templates used in page construction
 	temp := template.Must(template.ParseFiles("templates/index.html", "templates/navbar.html", "templates/project.html"))
 
 	log.Info("fetching main")
 
+	// parse page data
 	age := time.Now().Year() - 2004
 	if time.Now().Month() == time.January && time.Now().Day() < 5 {
 		age -= 1
@@ -33,6 +36,7 @@ func GetLanding(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
+	// Assemble page info
 	MainInfo := struct {
 		Age      string
 		Projects []db.Project
@@ -41,6 +45,7 @@ func GetLanding(w http.ResponseWriter, r *http.Request) {
 		projects,
 	}
 
+	// Build the page
 	if err := temp.Execute(w, MainInfo); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		log.WithFields(log.Fields{
