ca6f649083326174dc02ed95f574dfa1d82be47c
Ben Shirley <benshirley04@gmail.com>
2024-10-24 09:08:41 +1300

    implemented GetAllComments method

--- backend/internal/handlers/comments.go
+++ backend/internal/handlers/comments.go
@@ -5,11 +5,38 @@ This file contains the handlers for the /api/comments/ endpoint.
 */
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	database "server/internal/db"
+)
 
 // returns a json list of all comment ids
 func GetAllCommentIds(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
+	comments, err := database.GetAllCommentIds()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	encoding, err := json.Marshal(comments)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("content-type", "application/json")
+	_, err = w.Write(encoding)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
 }
 
 // returns a single json object containing a comment
