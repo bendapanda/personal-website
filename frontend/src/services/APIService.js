94210d621542b014205967c35905031ddc07daed
Ben Shirley <benshirley04@gmail.com>
2024-10-24 15:55:11 +1300

    added comment get request services

--- frontend/src/services/APIService.js
+++ frontend/src/services/APIService.js
@@ -1,12 +1,53 @@
+/**
+ * Ben Shirley 2024
+ * handles the communication with the api enpoints published by the go server
+ * 
+ */
 
+
+/**
+ * 
+ * @returns An array of project elements
+ */
 export const getProjects = async () => {
-    const response = await fetch("http://localhost:8080/api/projects");
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/api/projects`);
 
     if (!response.ok) {
         throw new Error(`HTTP error getting projects! ${response.status}`);
     }
-
-    const data = await response.json();
     
+    const data = await response.json();
     return data;
+}
+
+
+/**
+ *  Prompts the server for a list of the ids of the comments in the database 
+ * @returns A list of all comment ids in the database
+ */
+export const getCommentIds = async () => {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/api/comments/all`)
+
+    if (!response.ok) {
+        throw new Error(`HTTP error getting comments! ${response.status}`);
+    }
+    const data = await response.json();
+    return data
+}
+
+/**
+ * prompts the server for a specific comment by its id
+ * @param { int } id the id of the desired comment 
+ * @returns an object representation of the comment
+ */
+export const getComment = async (id) => {
+    const url = URL(`${process.env.REACT_APP_API_URL}/api/comments`);
+    url.searchParams.append("id", id);
+
+    const response = await fetch(url)
+    if (!response.ok) {
+        throw new Error(`HTTP error getting target comment! ${response.status}`);
+    }
+    const data = await response.json();
+    return data
 }
