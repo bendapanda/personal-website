4be473b6b9df5defdf5232b78eed1db5a23cde32
Ben Shirley <benshirley04@gmail.com>
2024-10-27 13:11:37 +1300

    tidied up carosel

--- frontend/src/components/Project.js
+++ frontend/src/components/Project.js
@@ -59,17 +59,16 @@ const ProjectCarousel = () => {
 const Project = ({ project }) => {
     return (
         
-        <div class="project">
+        <a class="project" href={project.URL}>
             <div class="project-left">
                 <h3 class="project-title">{ project.Name }</h3>
                 <p class="dates">{ project.Started } to { project.Finished }</p>
                 <p class="project-desc">{ project.Description }</p>
-                <a href={ project.Url }>Project Link</a>
             </div>
             <div class="project-right">
                 <img src={ `${process.env.PUBLIC_URL}${project.ImageFile }` } alt=""/>
             </div>
-        </div>
+        </a>
     )
 };
 
