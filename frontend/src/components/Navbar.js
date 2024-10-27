b84fe540412668cf1dae5842005f69c0356feef8
Ben Shirley <benshirley04@gmail.com>
2024-10-27 14:51:02 +1300

    created hanging ben effect

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles/Navbar.css';
 
 
 const Navbar = () => {
+    const [showHangingBen, setShowHangingBen] = useState(false);
+
     return (
         <ul class="navbar header">
             <li><h1>Ben Shirley</h1></li>
             <li><a class="navbar-link" href="/">Home</a></li>
-            <li><a class="navbar-link" href="/resume">Resume</a></li>
+            <li>
+                <a id="resume-link" class="navbar-link" href="/resume"
+                    onMouseEnter={() => setShowHangingBen(true)}
+                    onMouseLeave={() => setShowHangingBen(false)} 
+                >Resume</a>
+                {
+                    showHangingBen &&
+                    <img id="hanging-image" src={`${process.env.PUBLIC_URL}/resources/ben_hanging1.png`}/>
+                }
+                </li>
             <li><a class="navbar-link" href="https://github.com/bendapanda">Projects</a></li>
         </ul>
     );
