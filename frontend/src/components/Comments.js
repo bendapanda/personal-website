51c75d0da6aebd5c9b2c9c5366bbd349ea156202
Ben Shirley <benshirley04@gmail.com>
2024-10-30 20:44:25 +1300

    added ben in comments

--- frontend/src/components/Comments.js
+++ frontend/src/components/Comments.js
@@ -9,6 +9,7 @@ const CommentsSection = () => {
     const [page, setPage] = useState(1);
     const [commentIds, setCommentIds] = useState([]);
     const [currentComments, setCurrentComments] = useState([]);
+    const [showHiddenBen, setShowHiddenBen] = useState(false);
     const commentsPerPage = 5;
 
     useEffect(() => {
@@ -44,16 +45,31 @@ const CommentsSection = () => {
 
     return (<div className="comment-section"> 
         {
-            currentComments.map((comment) => {
-                return <Comment key={comment.Id} comment={comment}/>;
+            currentComments.map((comment, index) => {
+                // As a fun feature, We add a secret me that appears on hover on the second comment
+                if (index == 1) {
+                    console.log("yep")
+                    return (
+                        <div style={{position: "relative"}}>
+                            {
+                                showHiddenBen &&
+                                <img id="hidden-ben-comments" src={`${process.env.PUBLIC_URL}/resources/ben_squat.png`}/>
+                            }
+                            <div style={{zIndex: "1"}} onMouseEnter={() => setShowHiddenBen(true)} onMouseLeave={() => setShowHiddenBen(false)}>
+                                <Comment key={comment.Id} comment={comment} layer={2}/>
+                            </div>
+                        </div>
+                    );
+                }
+                return <Comment key={comment.Id} comment={comment} layer={0}/>;
             })
         }
     </div>);
 }
 
-const Comment = ({ comment }) => {
+const Comment = ({ comment, layer }) => {
     return (
-        <div className="comment-container">
+        <div className="comment-container" style={{position: "relative", zIndex: layer}}>
             <div className="comment-info">
                 <h3 className="comment-commenter">{comment.Commenter}</h3>
                 <h4 className="comment-date">{new Date(comment.Timestamp).toDateString()}</h4>
